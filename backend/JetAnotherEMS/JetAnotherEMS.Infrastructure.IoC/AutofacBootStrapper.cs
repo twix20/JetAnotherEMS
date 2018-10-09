cbd1abf802f5e73234f4ecc581f1628dfda2b371
Unknown <leereek@gmail.com>
2018-10-09 19:51:51 +0200

    webapi setup!

--- backend/JetAnotherEMS/JetAnotherEMS.Infrastructure.IoC/AutofacBootStrapper.cs
+++ backend/JetAnotherEMS/JetAnotherEMS.Infrastructure.IoC/AutofacBootStrapper.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Autofac;
 
 namespace JetAnotherEMS.Infrastructure.IoC
 {
     public class AutofacBootStrapper
     {
+        public static ContainerBuilder RegisterServices(ContainerBuilder builder)
+        {
+
+
+            return builder;
+        }
     }
 }
--- backend/JetAnotherEMS/JetAnotherEMS/Startup.cs
+++ backend/JetAnotherEMS/JetAnotherEMS/Startup.cs
@@ -1,17 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using JetAnotherEMS.Infrastructure.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
-namespace JetAnotherEMS
+namespace JetAnotherEMS.WebApi
 {
     public class Startup
     {
@@ -20,17 +19,34 @@ namespace JetAnotherEMS
             Configuration = configuration;
         }
 
+        public IContainer ApplicationContainer { get; private set; }
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "JetAnotherEMS API", Version = "v1" });
+            });
+
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+            AutofacBootStrapper.RegisterServices(builder);
+            ApplicationContainer = builder.Build();
+
+            return new AutofacServiceProvider(this.ApplicationContainer);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, IHostingEnvironment env)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -40,8 +56,17 @@ namespace JetAnotherEMS
                 app.UseHsts();
             }
 
+            app.UseStaticFiles();
+            app.UseAuthentication();
+
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "JetAnotherEMS Project API v1");
+            });
         }
     }
 }

f422e1f8c8b738f53687bd020820e097064ce763
twix20 <leereek@gmail.com>
2018-10-15 20:20:16 +0200

    Identity server with cert

---
+++ backend/JetAnotherEMS.Infrastructure.Identity/Models/AccountViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JetAnotherEMS.Infrastructure.Identity.Models.AccountViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+
+        [Display(Name = "RememberMe")]
+        public bool RememberMe { get; set; }
+    }
+}

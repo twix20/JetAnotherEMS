06626660d968834bec9955eb3494b2ce01b4e3e9
Unknown <leereek@gmail.com>
2018-11-18 21:46:23 +0100

    production url

--- frontend/src/config/urls.js
+++ frontend/src/config/urls.js
@@ -1,3 +1,3 @@
-export const API_HOST_URL = 'https://jetanotheremsapi-jqxuprouau.now.sh';
+export const API_HOST_URL = 'https://jetanotheremsapi-cajpqhipoi.now.sh';
 //export const API_HOST_URL = 'http://localhost:5000';
 export const API_URL = `${API_HOST_URL}/api`;
